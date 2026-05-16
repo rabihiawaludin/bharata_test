8d9b129644633959197cbaa36217a4fdc62e3683
rabihi <rabihi@suitmedia.com>
2026-05-16 14:46:20 +0700

    fixing k6

--- k6-test.js
+++ k6-test.js
@@ -12,7 +12,7 @@ export const options = {
 export default function () {
 
     const payload = JSON.stringify({
-        item_id: 3,
+        item_id: 1,
         tipe: 'keluar',
         qty: 1,
         user_id: 1,
@@ -25,7 +25,7 @@ export default function () {
     };
 
     const res = http.post(
-        'http://127.0.0.1:8000/api/transactions',
+        'https://bharata-test.bidev.my.id/api/transactions',
         payload,
         params
     );
